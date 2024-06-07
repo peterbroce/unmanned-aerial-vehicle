9267d26abde76f147f105afeafe2a03011a0fa7c
peterbroc <1635333133@qq.com>
2024-06-07 23:37:05 +0800

    添加页面样式

--- js/index.js
+++ js/index.js
@@ -1,14 +1,7 @@
 var clickBtn = function (t) {
-  document.querySelector(".type").innerHTML = t;
-  document.querySelector(".showhref").innerHTML = window.top.location.href;
-  if (window.top.location.href) {
-    if (window.top.location.href.indexOf("192.168.232.137") + 1) {
-      document.querySelector(".isHasHref").innerHTML = "域名一致";
-    } else {
-      document.querySelector(".isHasHref").innerHTML = "域名不一致";
-    }
-  }
-  setTimeout(function() {
-    window.top.location.href = `http://192.168.232.137/${t}/index.html`;
-  }, 6000);
+  window.top.location.href = window.top.location.href.split("index.html")[0] + `${t}/index.html`
 }
+
+var goback = function (t) {
+  window.top.location.href = window.top.location.href.split(t+"/").join("")
+};
